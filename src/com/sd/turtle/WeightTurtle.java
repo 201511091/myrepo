b2ff436416471052c0ad685b0eb3c2476930eac1
B-Sus <ster8@naver.com>
2016-03-14 17:56:35 +0900

    20160314

---
+++ src/com/sd/turtle/WeightTurtle.java
@@ -0,0 +1,20 @@
+/*
+ * @author ehk
+ * @since 20160314
+ * upcasting & downcasting study
+*/
+
+package com.sd.turtle;
+import ch.aplu.turtle.*;
+import java.awt.Color;
+
+public class WeightTurtle extends Turtle {
+  private int weight;
+  public WeightTurtle(int w){
+    weight = w;
+    setPenColor(Color.red);
+  }
+  public int getWeight() {
+    return weight;
+  }
+}
