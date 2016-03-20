ea80ad5f0ecb9c386465da48f3810403c59cf65d
Eunho Kim <ster8@naver.com>
2016-03-20 23:17:44 +0900

    week3 assignment

--- src/headfirst/strategy/MallardDuck.java
+++ src/headfirst/strategy/MallardDuck.java
@@ -1,11 +1,16 @@
 package headfirst.strategy;
 
 public class MallardDuck extends Duck {
-  public MallardDuck (){
-    quackBehavior = new Quack();
-    flyBehavior = new FlywithWings();
-  }
-  public void display(){
-    System.out.println("I am Mallrad duck");
-  }
+ 
+	public MallardDuck() {
+ 
+		quackBehavior = new Quack();
+                flyBehavior = new FlyWithWings();
+ 
+
+	}
+ 
+	public void display() {
+		System.out.println("I'm a real Mallard duck");
+	}
 }
